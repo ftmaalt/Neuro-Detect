93c0af7a5799c20932097e6e22cbd8c67f70e64e
ftmaalt <fmabdulla229@gmail.com>
2026-05-04 17:38:23 +0300

    update app.py

--- app.py
+++ app.py
@@ -19,14 +19,41 @@ if "disclaimer_accepted" not in st.session_state:
 # css
 
 def apply_style(theme):
-    bg_color = "#0E1117" if theme == 'dark' else "#FFFFFF"
-    text_color = "#FFFFFF" if theme == 'dark' else "#000000"
-    card_bg = "#1B212C" if theme == 'dark' else "#F0F2F6"
-    border_color = "#2D3748" if theme == 'dark' else "#D1D5DB"
+    if st.session_state.theme == "dark":
+        bg_color = "#0E1117"
+        text_color = "#FAFAFA"
+        card_bg = "#262730"
+        border_color = "#3A3A45"
+    else:
+        bg_color = "#FFFFFF"
+        text_color = "#111827"
+        card_bg = "#F3F4F6"
+        border_color = "#D1D5DB"
     
-    
-    st.markdown(f"""
-        <style>
+        st.markdown(f"""
+    <style>
+
+    /* main bg theme */
+    html, body, [data-testid="stAppViewContainer"], .stApp {{
+        background-color: {bg_color} !important;
+        color: {text_color} !important;
+    }}
+
+    /* upload container */
+    [data-testid="stFileUploader"] section {{
+        background-color: {card_bg} !important;
+        border-radius: 12px !important;
+        border: 1px solid {border_color} !important;
+    }}
+
+    /* upload button */
+    [data-testid="stFileUploader"] button {{
+        background-color: #111827 !important;
+        color: white !important;
+        border-radius: 8px !important;
+    }}
+
+
         .stApp {{ background-color: {bg_color}; color: {text_color}; }}
         st.html{{
     scroll-behavior: smooth;
@@ -298,7 +325,7 @@ elif st.session_state.view == 'portal':
                     st.write("Classifying pathology...")
                     idx = np.argmax(preds[0])
                     confidence = np.max(preds[0]) * 100
-                    if confidence < 75:
+                    if confidence < 80:
                         status.update(label="❌ Invalid Scan", state="error", expanded=False)
                         st.error("Invalid scan. Please upload a correct brain MRI scan.")
                         st.stop()
@@ -331,4 +358,4 @@ elif st.session_state.view == 'portal':
                 time.sleep(2.5)
                 brain_placeholder.empty()
 
-st.markdown("<br><hr><center><p style='color: #666;'>Senior Research Project 2026 | NeuroDetect AI</p></center>", unsafe_allow_html=True)
+st.markdown("<br><hr><center><p style='color: #666;'>Senior Project 2026 | NeuroDetect AI</p></center>", unsafe_allow_html=True)
